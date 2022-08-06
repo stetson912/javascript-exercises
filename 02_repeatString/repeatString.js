1d412881f81648792482197bd005dc48098b5857
stetson912 <trenton.stetson@gmail.com>
2022-08-06 11:14:34 -0700

    Solve all 7 tests in 02_repeatString javascript exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
 
+    let repeatedString = ""
+    if(num <0){return "ERROR"}
+    if(str && typeof str == "string"){
+        for(let i = 1; i <= num; i++){
+            repeatedString = repeatedString + str
+        }
+    }
+
+    return repeatedString
 };
 
 // Do not edit below this line
