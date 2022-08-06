872c161f433a1d3afd7295241f6c205c0fa85038
stetson912 <trenton.stetson@gmail.com>
2022-08-06 11:57:44 -0700

    Solve tests in 05_sumAll javascript exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0){
+        return "ERROR"
+    }
+    if(typeof a != 'number' || typeof b != 'number'){
+        return "ERROR"
+    }
 
+    let min = (a<b)? a : b
+    let max = (min == a) ? b : a
+    
+    let i = max; 
+    let sum = 0
+    while(i >= min){
+        sum+= i
+        i--
+    }
+    return sum
 };
 
 // Do not edit below this line
