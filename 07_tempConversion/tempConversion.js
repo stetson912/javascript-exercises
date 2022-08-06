62b013256f6a7ed9c2cbfd87a648da30238507e2
stetson912 <trenton.stetson@gmail.com>
2022-08-06 12:28:53 -0700

    Solve tests 07_tempConversion javascripts exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+//	from Celsius	to Celsius
+//Fahrenheit	[°F] = [°C] × 9⁄5 + 32	[°C] = ([°F] − 32) × 5⁄9
 
+const ftoc = function(fTemp) {
+  let celsiusTemp = (fTemp - 32) * (5 / 9 )
+
+  return (celsiusTemp == 0) ? 0 : parseFloat(celsiusTemp.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fahrenheitTemp = (cTemp * (9/5)) + 32
+  return (fahrenheitTemp == 0) ? 0 : parseFloat(fahrenheitTemp.toFixed(1))
 };
 
 // Do not edit below this line
