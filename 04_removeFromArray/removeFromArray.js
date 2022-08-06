77736b4793c42856cf86731d2b8c26b8c736972a
stetson912 <trenton.stetson@gmail.com>
2022-08-06 11:45:40 -0700

    Solve tests for 04_removeFromArray javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    let args = [...arguments]
 
+    args.forEach(argument => {
+        if(arr.includes(argument)){
+            let index = arr.indexOf(argument)
+            arr.splice(index, 1)
+        }
+    })
+
+    return arr
 };
 
 // Do not edit below this line
