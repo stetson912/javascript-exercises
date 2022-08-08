52778a9ff6b840e12f799f61eab442d221900ee8
stetson912 <trenton.stetson@gmail.com>
2022-08-08 11:33:21 -0700

    Solve 09 fibonacci javascript exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    //1,1,2,3,5,8...
+    n = parseInt(n)
+    if(n < 0){
+        return "OOPS"
+    }
+    if(n <=1){
+        return n
+    }
+    return fibonacci(n-1) + fibonacci(n-2)
+    
 };
 
 // Do not edit below this line
