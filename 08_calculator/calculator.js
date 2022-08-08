af3df468a86394ad568be3b62ff79979f6f977d3
stetson912 <trenton.stetson@gmail.com>
2022-08-08 10:20:58 -0700

    Solve 08 calculator javascript exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return (nums.length === 0)? 0 : (nums.length == 1)? nums[0] :
+	  nums.reduce((a,b) => {
+    return  a+b
+    },0 )
+
+
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return (nums.length === 0)? 0 : (nums.length == 1)? nums[0] :
+	  nums.reduce((a,b) => {
+    return  a*b
+    },1 )
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  return (n===0 || n===1)? 1: n * factorial(n-1)
 	
 };
 
