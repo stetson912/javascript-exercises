665619ea8089af36c97a09a6529714e9ddd8a640
stetson912 <trenton.stetson@gmail.com>
2022-08-08 11:09:05 -0700

    solve 09 palindromes javascript exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let filteredStr = str.toLowerCase().split("").filter( ch => {
+        return /\w/.test(ch)
+    }).join("")
+    
+    let reverseStr = filteredStr
+    reverseStr = reverseStr.split("").reverse().join("")
+
+    return(reverseStr === filteredStr)
 };
-
 // Do not edit below this line
 module.exports = palindromes;
